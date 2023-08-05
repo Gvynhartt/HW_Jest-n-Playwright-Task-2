00a19e27fb2650d31bfb830cfca5ae456d5b9e57
Gvynhartt <savshush134@gmail.com>
2023-08-05 05:43:06 +0300

    task 2 complete

---
+++ gui/tests/loginSuccessfulTest.spec.js
@@ -0,0 +1,22 @@
+
+const {test, expect} = require('@playwright/test');
+const { authInfo } = require('../../user');
+
+test('test', async ({page}) => {
+  
+  await page.goto('https://netology.ru/?modal=sign_in');
+
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill(authInfo.userLogin);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill(authInfo.userPassword);
+  await page.getByTestId('login-submit-btn').click();
+
+  await expect(page).toHaveURL("https://netology.ru/profile");
+  await expect(page.locator('xpath=//div[@data-testid="header-navigatorBtn"]').locator('nth=0')).toBeVisible();
+  await expect(page.locator('xpath=//div[@data-testid="header-navigatorBtn"]').locator('nth=0')).toHaveText("Каталог курсов");
+
+  // await expect(page.locator('xpath=(//a[@data-testid="profile-program-link"]//h3)[last()]')).toContainText(authInfo.userCourseName);
+  // раз мы всё равно берём реальные данные для авторизации, почему бы не использовать и название курса?
+  // вариант закомментирован, т. к. его сложно проверить тем, кто не студент "Нетологии"
+});
---
+++ gui/tests/loginUnsucessfulTests.spec.js
@@ -0,0 +1,19 @@
+
+const {test, expect} = require('@playwright/test');
+const { authInfo } = require('../../user');
+
+test('test', async ({page}) => {
+  
+  await page.goto('https://netology.ru/?modal=sign_in');
+
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill("3450d" + authInfo.userLogin);
+  await page.getByPlaceholder('Пароль').click();
+  await page.getByPlaceholder('Пароль').fill("aboba" + authInfo.userPassword);
+  await page.getByTestId('login-submit-btn').click();
+
+  await expect(page).toHaveURL("https://netology.ru/?modal=sign_in");
+  const errorPopup = page.getByTestId('login-error-hint');
+  await expect(errorPopup).toBeVisible();
+  await expect(errorPopup).toHaveText("Вы ввели неправильно логин или пароль");
+});
